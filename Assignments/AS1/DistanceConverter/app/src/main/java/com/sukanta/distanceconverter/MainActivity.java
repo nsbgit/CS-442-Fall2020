cae45eb0dffdc575b36a9dfb788e906f0d18dc34
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-09-16 19:30:43 +0530

    radio group function updated

--- Assignments/AS1/DistanceConverter/app/src/main/java/com/sukanta/distanceconverter/MainActivity.java
+++ Assignments/AS1/DistanceConverter/app/src/main/java/com/sukanta/distanceconverter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sukanta.distanceconverter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -27,21 +28,40 @@ public class MainActivity extends AppCompatActivity {
     private EditText etInputValue;
     private Button bConvert, bClear;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createReferenceForScreenElements();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        // Code here
+
+        // Call super last
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        // Call super first
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Code here
     }
 
     public void rgMiKm_Clicked(View v) {
         switch (v.getId()) {
             case R.id.rbMiToKm:
                 typeOfConversion = TYPE_OF_CONVERSION.MILES_TO_KILOMETERS;
+                tvInputLable.setText(R.string.miles_value);
+                tvOutputLable.setText(R.string.kilometers_value);
                 break;
             case R.id.rbKmToMi:
                 typeOfConversion = TYPE_OF_CONVERSION.KILOMETERS_TO_MILES;
+                tvInputLable.setText(R.string.kilometers_value);
+                tvOutputLable.setText(R.string.miles_value);
                 break;
         }
     }
@@ -50,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void bClear_Clicked(View v) {}
 
-    private void CreateReferenceForScreenElements() {
+    private void createReferenceForScreenElements() {
         rgMiKm = findViewById(R.id.rgMiKm);
         rbMiToKm = findViewById(R.id.rbMiToKm);
         rbKmToMi = findViewById(R.id.rbKmToMi);
@@ -64,30 +84,17 @@ public class MainActivity extends AppCompatActivity {
         bClear = findViewById(R.id.bClear);
     }
 
-    private void SetLableText() {
-        switch (typeOfConversion) {
-            case KILOMETERS_TO_MILES:
-                tvInputLable.setText("Kilometers Value:");
-                tvOutputLable.setText("Miles Value:");
-                break;
-            case MILES_TO_KILOMETERS:
-                tvInputLable.setText("Miles Value:");
-                tvOutputLable.setText("Kilometers Value:");
-                break;
-        }
-    }
-
     public float calculate(float input) {
         float conversionFactorMilesToKilometers = 1.60934f;
         float conversionFactorKilometersToMiles = 0.621371f;
         float result = 0.0f;
 
         switch (typeOfConversion) {
-            case KILOMETERS_TO_MILES:
-                result = input * conversionFactorKilometersToMiles;
-                break;
             case MILES_TO_KILOMETERS:
                 result = input * conversionFactorMilesToKilometers;
+                break;
+            case KILOMETERS_TO_MILES:
+                result = input * conversionFactorKilometersToMiles;
                 break;
         }
 
