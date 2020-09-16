a08aaca7fa672c38532b00fc89ca17f709bd32a9
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-09-16 21:26:47 +0530

    Convert Functionality added

--- Assignments/AS1/DistanceConverter/app/src/main/java/com/sukanta/distanceconverter/MainActivity.java
+++ Assignments/AS1/DistanceConverter/app/src/main/java/com/sukanta/distanceconverter/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
         KILOMETERS_TO_MILES
     }
 
-    TYPE_OF_CONVERSION typeOfConversion = TYPE_OF_CONVERSION.MILES_TO_KILOMETERS;
+    private TYPE_OF_CONVERSION typeOfConversion = TYPE_OF_CONVERSION.MILES_TO_KILOMETERS;
+    private String converstionTypeString = "Mi to Km: ";
+    private String TAG = "MainActivity";
 
     // reference of screen elements
     private RadioGroup rgMiKm;
@@ -52,52 +58,112 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void rgMiKm_Clicked(View v) {
-        switch (v.getId()) {
-            case R.id.rbMiToKm:
-                typeOfConversion = TYPE_OF_CONVERSION.MILES_TO_KILOMETERS;
-                tvInputLable.setText(R.string.miles_value);
-                tvOutputLable.setText(R.string.kilometers_value);
-                break;
-            case R.id.rbKmToMi:
-                typeOfConversion = TYPE_OF_CONVERSION.KILOMETERS_TO_MILES;
-                tvInputLable.setText(R.string.kilometers_value);
-                tvOutputLable.setText(R.string.miles_value);
-                break;
+        try {
+            switch (v.getId()) {
+                case R.id.rbMiToKm:
+                    typeOfConversion = TYPE_OF_CONVERSION.MILES_TO_KILOMETERS;
+                    tvInputLable.setText(R.string.miles_value);
+                    tvOutputLable.setText(R.string.kilometers_value);
+                    converstionTypeString = "Mi to Km: ";
+                    break;
+                case R.id.rbKmToMi:
+                    typeOfConversion = TYPE_OF_CONVERSION.KILOMETERS_TO_MILES;
+                    tvInputLable.setText(R.string.kilometers_value);
+                    tvOutputLable.setText(R.string.miles_value);
+                    converstionTypeString = "Km to Mi: ";
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "Something went wrong.",
+                    Toast.LENGTH_SHORT).show();
+            etInputValue.setText("");
+            Log.e(TAG, "rgMiKm_Clicked: ", e);
         }
     }
 
-    public void bConvert_Clicked(View v) {}
+    public void bConvert_Clicked(View v) {
+        try {
+            String inputString = etInputValue.getText().toString();
+
+            if (inputString == null || inputString.equals("")) {
+                Toast.makeText(getApplicationContext(),
+                        "Please enter a value.",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            etInputValue.setText("");
+            double inputValue = Double.parseDouble(inputString);
+            double resultValue = calculate(inputValue);
+
+            String pattern = "0.0";
+            char leftArrow = '\u2794';
+            DecimalFormat numberFormat = new DecimalFormat(pattern);
+            String resultString = numberFormat.format(resultValue);
+            String historyString = converstionTypeString + resultString + " " + leftArrow + " " + resultString;
+            addToHistory(historyString);
+            tvOutputValue.setText(resultString);
+        } catch (NumberFormatException e) {
+            //e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "Number is not in proper format.",
+                    Toast.LENGTH_SHORT).show();
+            etInputValue.setText("");
+            Log.e(TAG, "bConvert_Clicked: ", e);
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "Something went wrong.",
+                    Toast.LENGTH_SHORT).show();
+            etInputValue.setText("");
+            Log.e(TAG, "bConvert_Clicked: ", e);
+        }
+    }
+
+    private void addToHistory(String historyString) {
+    }
 
     public void bClear_Clicked(View v) {}
 
     private void createReferenceForScreenElements() {
-        rgMiKm = findViewById(R.id.rgMiKm);
-        rbMiToKm = findViewById(R.id.rbMiToKm);
-        rbKmToMi = findViewById(R.id.rbKmToMi);
-        tvInputLable = findViewById(R.id.tvInputLable);
-        tvOutputLable = findViewById(R.id.tvOutputLable);
-        tvOutputValue = findViewById(R.id.tvOutputValue);
-        tvConversionHistory = findViewById(R.id.tvConversionHistory);
-        tvConversionHistory.setMovementMethod(new ScrollingMovementMethod());
-        etInputValue = findViewById(R.id.etInputValue);
-        bConvert = findViewById(R.id.bConvert);
-        bClear = findViewById(R.id.bClear);
+        try {
+            rgMiKm = findViewById(R.id.rgMiKm);
+            rbMiToKm = findViewById(R.id.rbMiToKm);
+            rbKmToMi = findViewById(R.id.rbKmToMi);
+            tvInputLable = findViewById(R.id.tvInputLable);
+            tvOutputLable = findViewById(R.id.tvOutputLable);
+            tvOutputValue = findViewById(R.id.tvOutputValue);
+            tvConversionHistory = findViewById(R.id.tvConversionHistory);
+            tvConversionHistory.setMovementMethod(new ScrollingMovementMethod());
+            etInputValue = findViewById(R.id.etInputValue);
+            bConvert = findViewById(R.id.bConvert);
+            bClear = findViewById(R.id.bClear);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "Something went wrong.",
+                    Toast.LENGTH_SHORT).show();
+            etInputValue.setText("");
+            Log.e(TAG, "createReferenceForScreenElements: ", e);
+        }
     }
 
-    public float calculate(float input) {
-        float conversionFactorMilesToKilometers = 1.60934f;
-        float conversionFactorKilometersToMiles = 0.621371f;
-        float result = 0.0f;
+    public double calculate(double inputValue) {
+        double conversionFactorMilesToKilometers = 1.60934;
+        double conversionFactorKilometersToMiles = 0.621371;
+        double resultValue = 0.0;
 
         switch (typeOfConversion) {
             case MILES_TO_KILOMETERS:
-                result = input * conversionFactorMilesToKilometers;
+                resultValue = inputValue * conversionFactorMilesToKilometers;
                 break;
             case KILOMETERS_TO_MILES:
-                result = input * conversionFactorKilometersToMiles;
+                resultValue = inputValue * conversionFactorKilometersToMiles;
                 break;
         }
 
-        return result;
+        return resultValue;
     }
 }
