cfe5c83ddcc666db32bbf9c94d2f7da572035383
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-09-29 02:32:23 +0530

    menu added

--- Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/MainActivity.java
+++ Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/MainActivity.java
@@ -1,8 +1,12 @@
 package com.sukanta.multinotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mInfo:
+                Toast.makeText(this, "Info Clicked", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.mAdd:
+                Toast.makeText(this, "Add Clicked", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
