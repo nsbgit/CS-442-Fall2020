77b3e94177a0eeaf3ecbe3c29b4dae9ecd484622
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-09-29 05:05:24 +0530

    Edit Activity Menu Added

---
+++ Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/EditActivity.java
@@ -0,0 +1,35 @@
+package com.sukanta.multinotes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_edit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mAdd:
+                Toast.makeText(this, "Save Clicked", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
