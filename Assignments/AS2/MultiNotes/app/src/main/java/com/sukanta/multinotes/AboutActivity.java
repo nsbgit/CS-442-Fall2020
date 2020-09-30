aa31f2f08ede9e3f7f93dbf9226e71b46e85a694
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-09-30 22:15:02 +0530

    copyright text year

--- Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/AboutActivity.java
+++ Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/AboutActivity.java
@@ -5,6 +5,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
@@ -17,6 +21,13 @@ public class AboutActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         TextView tvCopyright = findViewById(R.id.tvCopyright);
-        tvCopyright.setText(R.string.copyright);
+
+        Date date = new Date();
+        String format = "yyyy";
+        DateFormat dateFormat = new SimpleDateFormat(format, Locale.US);
+        String year = dateFormat.format(date);
+        String copyrightText = String.format(getString(R.string.copyright),year);
+
+        tvCopyright.setText(copyrightText);
     }
 }
