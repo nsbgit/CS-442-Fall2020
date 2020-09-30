62ed3d59e8d6f00fb7e2797b8e6cc55fe6f8f8be
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-09-30 21:27:09 +0530

    Update Note.java

--- Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/Note.java
+++ Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/Note.java
@@ -5,10 +5,7 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class Note implements Serializable {
