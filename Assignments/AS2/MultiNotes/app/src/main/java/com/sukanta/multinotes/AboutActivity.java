c5234d711ec0d9ca3bb53758b41514e08388a447
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-09-30 21:31:13 +0530

    Copyright

--- Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/AboutActivity.java
+++ Assignments/AS2/MultiNotes/app/src/main/java/com/sukanta/multinotes/AboutActivity.java
@@ -1,25 +1,22 @@
 package com.sukanta.multinotes;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-public class AboutActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private static final String TAG = "AboutActivity";
+import java.util.Objects;
+
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         TextView tvCopyright = findViewById(R.id.tvCopyright);
-        tvCopyright.setText("\u00a9" + "2020, Sukanta Sharma");
+        tvCopyright.setText(R.string.copyright);
     }
 }
