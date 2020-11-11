f2353ff7574bd3e5b62b5b6fb882adf1dbf20a0c
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-11-11 17:26:13 +0530

    Menu items added

--- Assignments/AS4/KnowYourGoverment/app/src/main/java/com/sukanta/knowyourgoverment/MainActivity.java
+++ Assignments/AS4/KnowYourGoverment/app/src/main/java/com/sukanta/knowyourgoverment/MainActivity.java
@@ -1,9 +1,13 @@
 package com.sukanta.knowyourgoverment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -17,6 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         downloadData("Chicago");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu: ");
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        try {
+            Log.d(TAG, "onOptionsItemSelected: ");
+            switch (item.getItemId()) {
+                case R.id.mInfo:
+                    Toast.makeText(getApplicationContext(),"Info Clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.mSearch:
+                    Toast.makeText(getApplicationContext(),"Search Clicked",Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "onOptionsItemSelected: ", e);
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     public void downloadData(String address) {
