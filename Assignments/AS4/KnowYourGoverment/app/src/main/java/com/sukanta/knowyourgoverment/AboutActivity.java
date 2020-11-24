f5548fd2637c0a400aa6595a5a946c65900175b9
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-11-24 11:52:39 +0530

    API Attribution (clickable link to https://developers.google.com/civicinformation/)

--- Assignments/AS4/KnowYourGoverment/app/src/main/java/com/sukanta/knowyourgoverment/AboutActivity.java
+++ Assignments/AS4/KnowYourGoverment/app/src/main/java/com/sukanta/knowyourgoverment/AboutActivity.java
@@ -2,7 +2,13 @@ package com.sukanta.knowyourgoverment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -13,6 +19,14 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        //Objects.requireNonNull(getSupportActionBar()).hide();
+        TextView linkText = findViewById(R.id.textView3);
+        SpannableString content = new SpannableString("Google Civic Information API");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        linkText.setText(content);
+    }
+
+    public void gotoGoogleCivicInformationApi(View v) {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://developers.google.com/civic-information/"));
+        startActivity(i);
     }
 }
