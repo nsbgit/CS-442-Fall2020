600abdb9c04ca4cabd1ad0579b57f64480f2722c
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-11-25 23:38:36 +0530

    PhotoDetailActivity added

---
+++ Assignments/AS4/KnowYourGoverment/app/src/main/java/com/sukanta/knowyourgoverment/PhotoDetailActivity.java
@@ -0,0 +1,14 @@
+package com.sukanta.knowyourgoverment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+    }
+}
